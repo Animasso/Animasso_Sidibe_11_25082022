138ec2d58dec3616684ebbdd6d9fe34a677f0183
Animasso Sidibe <asidibe017@gmail.com>
2022-09-03 17:10:22 +0200

    ajout use fetch

--- src/pages/HousingDetail.js
+++ src/pages/HousingDetail.js
@@ -1,22 +1,22 @@
-import { useEffect, useState } from "react";
+// import { useState } from "react";
 import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 function HousingDetail(props) {
-  const [locations, setLocations] = useState([]);
-  const [oneLocation, setOneLocation] = useState([]);
+  // const [oneLocation, setOneLocation] = useState([]);
   const params = useParams();
-  useEffect(() => {
-    const url =
-      "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json";
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setLocations(data));
+  console.log(params);
+  const url =
+    "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json";
+  const houses = useFetch(url);
+  console.log(houses);
 
-    // .then((listHouse) => setLocations(listHouse));
-    console.log(params);
-  }, [params]);
-  const house = locations.find((location) => location.id === params);
-  setOneLocation(house);
+  const oneHouse = houses.find((house) => house.id === params);
+  // const oneHouse = houses.find((house) => {
+  //   return house.id === params.id;
+  // });
+  console.log(oneHouse);
+  // setOneLocation(house);
 
   return (
     <div>
@@ -24,9 +24,9 @@ function HousingDetail(props) {
       <div className="singleLocation">
         <div className="first">
           <div className="locationTitlePosition">
-            <p className="title"></p>
-            <p className="location"></p>
-            <p className="tags"></p>
+            <p className="Onetitle">{oneHouse.title}</p>
+            <p className="location">{oneHouse.location}</p>
+            <p className="tags"> </p>
           </div>
         </div>
       </div>
