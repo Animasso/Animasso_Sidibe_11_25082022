231e03e26ecec608ba3ea39ab0278b58bad19d04
Animasso Sidibe <asidibe017@gmail.com>
2022-09-13 04:08:35 +0200

    Ajout du responsive

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,4 +1,8 @@
+import redStar from "../assets/RedStar.png";
+import greyStar from "../assets/GreyStar.png";
 function Stars(props) {
+  //   for (int i = 1;  i <= rate; i++) <EtoileRouge/>
+  // for (int i = 1; i <= (5 - rate); i++ <EtoileGrise/>
   return <div></div>;
 }
 
